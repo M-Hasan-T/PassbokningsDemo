a2c195b13ebe1470de9024eac4bf73824fa2f467
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-13 02:06:56 +0100

    Seeded admin-account

--- PassbokningsDemo/Program.cs
+++ PassbokningsDemo/Program.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PassbokningsDemo.Data;
+using PassbokningsDemo.Extensions;
 using PassbokningsDemo.Models;
 
 namespace PassbokningsDemo
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -35,6 +36,8 @@ namespace PassbokningsDemo
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            await app.SeedDataAsync();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
